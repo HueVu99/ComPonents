f0af1ff1756d41446fd007a1cc83e0e235813f36
HueVu99 <huevu.211001@gmail.com>
2021-11-12 15:33:25 +0700

    add source code search bar

---
+++ Side Navigation/main.js
@@ -0,0 +1,18 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
+// function openNav() {
+//   document.getElementById("mySidenav").style.width = "100%";
+// }
+
+// function closeNav() {
+//   document.getElementById("mySidenav").style.width = "0";
+// }
